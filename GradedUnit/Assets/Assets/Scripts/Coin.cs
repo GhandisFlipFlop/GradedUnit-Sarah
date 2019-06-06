8cf42b0b7bd767356c91ea5bf9677ca53c780a3f
GhandisFlipFlop <43068438+GhandisFlipFlop@users.noreply.github.com>
2019-06-06 10:06:34 +0100

    copied unity proejct

---
+++ GradedUnit/Assets/Assets/Scripts/Coin.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+
+    public Score scoreObject;
+    public int coinValue;
+
+    public AudioSource audioObject;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    // Unity calls this function when coin touches any other object
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if the thing we touched is the player
+        Player playerScript = collision.collider.GetComponent<Player>();
+
+        // If the thing we touched has a playerscript, that means it is the player
+        if (playerScript)
+        {
+            // We hit the player
+            // Add to the score based on our value
+            scoreObject.AddScore(coinValue);
+
+            //PLay audio clip on collision
+            audioObject.Play();
+
+            // Destroy the gameObject that this script is attached to (the coin)
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ GradedUnit/Assets/Assets/Scripts/Door.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// allows us to use scene management
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour {
+
+    public string sceneToLoad;
+    public string DefeatScreen;
+    public Score scoreObject;
+    public int scoreNeeded = 0;
+
+    //unity calls this function when something collides with player
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //check if the thing we collided with has a player script
+        Player playerScript = collision.collider.GetComponent<Player>();
+
+        //only do something if the thing we ran into has a player script
+        if (playerScript != null && scoreObject.GetScore() >= scoreNeeded)
+        {
+            //we DID hit the player
+
+            scoreObject.SaveScore();
+
+            //GO TO NEXT LEVEL
+            SceneManager.LoadScene(sceneToLoad);
+        }
+        else if (playerScript != null && scoreObject.GetScore() < scoreNeeded)
+        {
+            SceneManager.LoadScene(DefeatScreen);
+        }
+    }
+
+}
